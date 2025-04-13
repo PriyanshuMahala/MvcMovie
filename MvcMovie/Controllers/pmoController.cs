8e6455f84d10c46ea44fcc189ee1db51d7ad706b
PriyanshuMahala <mahalapriyanshu34@gmail.com>
2025-04-13 21:00:46 +0530

    Add project files.

---
+++ MvcMovie/Controllers/pmoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class pmoController : Controller
+    {
+        // GET: pmo
+        public string Index()
+        {
+            return "Ts s m dflt actn";
+        }
+
+        //pmo/welcome
+        public string Welcome()
+        {
+            return "Welcome lirila larila";
+        }
+    }
+}
